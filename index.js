45bf9b6c14080a6e8f48f605743d18ff18d35482
ZombieB <51412993+ZombieBrine13092@users.noreply.github.com>
2022-07-07 01:27:44 -0400

    QoL config & script additions
    rewrote 1/4 of the script along the way
    
    console clears now too

--- index.js
+++ index.js
@@ -13,6 +13,14 @@ if (!fs.existsSync('./config/config.yml')) {
     console.log(chalk.italic('./config/config.yml') + ' is not present!')
     process.exit();
 }
+
+// load config
+console.log('Loading config @ ./config/config.yml');
+var config = yaml.load(fs.readFileSync('./config/config.yml', 'utf8'));
+
+console.clear(); // After all.. why not? Why shouldn't I clear the console?
+
+// error catcher 2: electric boogaloo
 if (!fs.existsSync('./config/key.pem')) {
     console.log('Process terminating: File not found');
     console.log(chalk.italic('./config/key.pem') + ' is not present!')
@@ -24,21 +32,34 @@ if (!fs.existsSync('./config/cert.pem')) {
     process.exit();
 }
 
-// load config
-console.log('Loading config @ ./config/config.yml');
-var config = yaml.load(fs.readFileSync('./config/config.yml', 'utf8'));
-
 // hash up the management password
 var hash = crypto.createHash('md5').update(config.managePassword).digest('hex');
 
-// simpl logger
-function log(loc, address, code, agent) {
-    if (config.logging == true && config.simple == false) {
-        console.log(chalk.inverse(address) + ' ' + chalk.yellow(code) + ' ' + loc + ' ' + chalk.italic(chalk.gray('(' + agent + ')')));
-    } else if (config.logging == true && config.simple == true) {
-        console.log(chalk.gray(address) +  ' ' + loc);
-    } else if (config.logging == false) {
-        return;
+// logger function
+// works only when logging is set to true in the config
+function log(loc, address, code, agent, qLoc) {
+    if (config.hideQueryString == true) {
+        if (loc == '/favicon.ico' && config.hideGetFavicon == true) {
+            return;
+        }
+        if (config.logging == true && config.simple == false) {
+            console.log(chalk.inverse(address) + ' ' + chalk.yellow(code) + ' ' + loc + ' ' + chalk.italic(chalk.gray('(' + agent + ')')));
+        } else if (config.logging == true && config.simple == true) {
+            console.log(chalk.gray(address) +  ' ' + loc);
+        } else if (config.logging == false) {
+            return;
+        }
+    } else if (config.hideQueryString == false) {
+        if (loc == '/favicon.ico' && config.hideGetFavicon == true) {
+            return;
+        }
+        if (config.logging == true && config.simple == false) {
+            console.log(chalk.inverse(address) + ' ' + chalk.yellow(code) + ' ' + qLoc + ' ' + chalk.italic(chalk.gray('(' + agent + ')')));
+        } else if (config.logging == true && config.simple == true) {
+            console.log(chalk.gray(address) +  ' ' + qLoc);
+        } else if (config.logging == false) {
+            return;
+        }
     }
 }
 
@@ -50,33 +71,44 @@ app.get('/', (req, res) => {
     var code = 200;
     res.setHeader("Content-Type", "text/plain");
     res.send('Hello world!');
-    log(req.path, req.ip, code, req.get('User-Agent'));
+    log(req.path, req.ip, code, req.get('User-Agent'), req.originalUrl);
+});
+app.get('/ip', (req, res) => {
+    res.statusCode = 200;
+    var code = 200;
+    res.setHeader("Content-Type", "text/plain");
+    if (req.query.json == 'true') {
+        res.send('{"ip":"' + req.ip + '"}');
+    } else {
+        res.send(req.ip);
+    }
+    log(req.path, req.ip, code, req.get('User-Agent'), req.originalUrl);
 });
 app.get('/manage', (req, res) => {
     res.setHeader("Content-Type", "text/plain");
     if (req.query.pass !== undefined) {
         if (crypto.createHash('md5').update(req.query.pass).digest('hex') == hash) {
-            res.send('200 OK');
             var code = 200;
             res.statusCode = 200;
+            res.send('200 OK');
         } else {
-            res.send('401 Unauthorized');
-            var code = 401;
-            res.statusCode = 401;
+            var code = 400;
+            res.statusCode = 400;
+            res.send('400 Bad request');
         }
     } else {
-        res.send('401 Unauthorized');
         var code = 401;
         res.statusCode = 401;
+        res.send('401 Unauthorized');
     }
-    log(req.path, req.ip, code, req.get('User-Agent'));
+    log(req.path, req.ip, code, req.get('User-Agent'), req.originalUrl);
 });
 app.get('*', (req, res) => {
     res.statusCode = 404;
     var code = 404;
     res.setHeader("Content-Type", "text/plain");
     res.send('404 Not found');
-    log(req.path, req.ip, code, req.get('User-Agent'));
+    log(req.path, req.ip, code, req.get('User-Agent'), req.originalUrl);
 });
 
 // https.listen on config port
