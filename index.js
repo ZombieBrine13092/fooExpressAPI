7445347e7b2ba148ac092e3c04fc7aa8d0af0da7
ZombieB <51412993+ZombieBrine13092@users.noreply.github.com>
2022-07-07 16:27:25 -0400

    version 4 script

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // import shiz
-import express from 'express';
+import express, { text } from 'express';
 import https from 'https';
 import chalk from 'chalk';
 import crypto from 'crypto';
@@ -7,7 +7,10 @@ import fs from 'fs';
 import yaml from 'js-yaml';
 var app = express();
 
-// error catcher 9000
+// script version
+var version = 4.0;
+
+// error catcher 9000 (can check before config is loaded)
 if (!fs.existsSync('./config/config.yml')) {
     console.log('Process terminating: File not found');
     console.log(chalk.italic('./config/config.yml') + ' is not present!')
@@ -18,110 +21,119 @@ if (!fs.existsSync('./config/config.yml')) {
 console.log('Loading config @ ./config/config.yml');
 var config = yaml.load(fs.readFileSync('./config/config.yml', 'utf8'));
 
-console.clear(); // After all.. why not? Why shouldn't I clear the console?
+// error catcher 2: electric boogaloo (can only check after config is loaded)
+if (!fs.existsSync(config.certPath) && !fs.existsSync(config.keyPath)) {
+    console.log('Process terminating: Files not found');
+    console.log(chalk.italic(config.keyPath) + ' and ' + chalk.italic(config.certPath) + ' are not present!')
+    console.log('Change the config to properly define the location of these files.');
+    process.exit();
+}
+if (!fs.existsSync(config.keyPath)) {
+    console.log('Process terminating: File not found');
+    console.log(chalk.italic(config.keyPath) + ' is not present!')
+    console.log('Change the config to properly define the location of this file.');
+    process.exit();
+}
+if (!fs.existsSync(config.certPath)) {
+    console.log('Process terminating: File not found');
+    console.log(chalk.italic(certPath) + ' is not present!')
+    console.log('Change the config to properly define the location of this file.');
+    process.exit();
+}
+if (config.version) {
+    console.log('WARNING: Config version does not match script version!');
+} else {
+    console.log('Process terminating: Could not determine config version');
+    console.log('This could be caused by an older config that does not specify a version.');
+    process.exit();
+}
 
-// error catcher 2: electric boogaloo
-if (!fs.existsSync('./config/key.pem')) {
-    console.log('Process terminating: File not found');
-    console.log(chalk.italic('./config/key.pem') + ' is not present!')
-    process.exit();
-}
-if (!fs.existsSync('./config/cert.pem')) {
-    console.log('Process terminating: File not found');
-    console.log(chalk.italic('./config/cert.pem') + ' is not present!')
-    process.exit();
-}
+if (config.clearConsoleOnStart == true) {console.clear();} // clear console if config says so
 
 // hash up the management password
 var hash = crypto.createHash('md5').update(config.managePassword).digest('hex');
 
-// logger function
+// logger functions
 // works only when logging is set to true in the config
-function log(loc, address, code, agent, qLoc) {
-    if (config.hideQueryString == true) {
-        if (loc == '/favicon.ico' && config.hideGetFavicon == true) {
-            return;
-        }
-        if (config.logging == true && config.simple == false) {
-            console.log(chalk.inverse(address) + ' ' + chalk.yellow(code) + ' ' + loc + ' ' + chalk.italic(chalk.gray('(' + agent + ')')));
-        } else if (config.logging == true && config.simple == true) {
-            console.log(chalk.gray(address) +  ' ' + loc);
-        } else if (config.logging == false) {
-            return;
-        }
-    } else if (config.hideQueryString == false) {
-        if (loc == '/favicon.ico' && config.hideGetFavicon == true) {
-            return;
-        }
-        if (config.logging == true && config.simple == false) {
-            console.log(chalk.inverse(address) + ' ' + chalk.yellow(code) + ' ' + qLoc + ' ' + chalk.italic(chalk.gray('(' + agent + ')')));
-        } else if (config.logging == true && config.simple == true) {
-            console.log(chalk.gray(address) +  ' ' + qLoc);
-        } else if (config.logging == false) {
-            return;
-        }
+// address, code, loc, qLoc, agent
+function log(reqData) {
+    if (config.logging == false) {return;} // if logging is set to false, don't
+    if (config.hideGetFavicon == true && reqData[2] == '/favicon.ico' || reqData[3].includes("favicon.ico") == true) {return;} 
+    // ^ if hideGetFavicon is set to true and the request is for the favicon, don't
+    if (config.simplifiedLogging == false) {
+        console.log(chalk.inverse(reqData[0]) + ' ' + chalk.yellow(reqData[1]) + ' ' + getLoc(reqData) + ' ' + chalk.italic(chalk.gray('(' + reqData[4] + ')')));
+    } else if (config.simplifiedLogging == true) {
+        console.log(chalk.gray(reqData[0]) + ' ' + getLoc(reqData));
+    } else {
+        console.log('WARNING: simplifiedLogging is not true or false! Defaulting to false..');
+        console.log(chalk.inverse(reqData[0]) + ' ' + chalk.yellow(reqData[1]) + ' ' + getLoc(reqData) + ' ' + chalk.italic(chalk.gray('(' + reqData[4] + ')')));
     }
 }
+function getLoc(reqData) { // return value based upon hideQueryString in config
+    if (config.hideQueryString == true) {
+        return reqData[2];
+    } else if (config.hideQueryString == false) {
+        return reqData[3];
+    } else {
+        console.log('WARNING: hideQueryString is not true or false! Defaulting to true..')
+        return reqData[2];
+    }
+} // does not include query
 
 // routes
-// I know there's probably a better way to set the status code before sending a response
-// Shove that into my to-do list
 app.get('/', (req, res) => {
-    res.statusCode = 200;
-    var code = 200;
-    res.setHeader("Content-Type", "text/plain");
-    res.send('Hello world!');
-    log(req.path, req.ip, code, req.get('User-Agent'), req.originalUrl);
+    res.status(200).setHeader("Content-Type", "text/plain").send('{"status":"OK"}');
+    const reqData = [req.ip, 200, req.path, req.originalUrl, req.get('User-Agent')];
+    log(reqData);
 });
 app.get('/ip', (req, res) => {
-    res.statusCode = 200;
-    var code = 200;
-    res.setHeader("Content-Type", "text/plain");
-    if (req.query.json == 'true') {
-        res.send('{"ip":"' + req.ip + '"}');
+    if (req.query.type == 'json') {
+        res.status(200).setHeader("Content-Type", "text/plain").send('{"ip":"' + req.ip + '"}');
+        var code = 200;
+    } else if (req.query.type == 'plaintext') {
+        res.status(200).setHeader("Content-Type", "text/plain").send(req.ip);
+        var code = 200;
     } else {
-        res.send(req.ip);
+        res.sendStatus(400);
+        var code = 400;
     }
-    log(req.path, req.ip, code, req.get('User-Agent'), req.originalUrl);
+    const reqData = [req.ip, code, req.path, req.originalUrl, req.get('User-Agent')];
+    log(reqData);
 });
 app.get('/manage', (req, res) => {
-    res.setHeader("Content-Type", "text/plain");
     if (req.query.pass !== undefined) {
         if (crypto.createHash('md5').update(req.query.pass).digest('hex') == hash) {
+            res.status(200).setHeader("Content-Type", "text/plain").send('foobar management page');
             var code = 200;
-            res.statusCode = 200;
-            res.send('200 OK');
+            // do management stuff
         } else {
+            res.sendStatus(400);
             var code = 400;
-            res.statusCode = 400;
-            res.send('400 Bad request');
         }
     } else {
-        var code = 401;
-        res.statusCode = 401;
-        res.send('401 Unauthorized');
+        res.sendStatus(400);
+        var code = 400;
     }
-    log(req.path, req.ip, code, req.get('User-Agent'), req.originalUrl);
+    const reqData = [req.ip, code, req.path, req.originalUrl, req.get('User-Agent')];
+    log(reqData);
 });
 app.get('*', (req, res) => {
-    res.statusCode = 404;
-    var code = 404;
-    res.setHeader("Content-Type", "text/plain");
-    res.send('404 Not found');
-    log(req.path, req.ip, code, req.get('User-Agent'), req.originalUrl);
+    res.sendStatus(404);
+    const reqData = [req.ip, 404, req.path, req.originalUrl, req.get('User-Agent')];
+    log(reqData);
 });
 
 // https.listen on config port
 https.createServer(
-    {key: fs.readFileSync('./config/key.pem'),cert: fs.readFileSync('./config/cert.pem')},app
-    ).listen(config.port, config.address);
+    {key: fs.readFileSync(String(config.keyPath)),cert: fs.readFileSync(String(config.certPath))},
+    app).listen(config.port, config.address);
 console.log('Server listening on ' + chalk.inverse(config.address + ':' + config.port));
 // log config status
 if (config.logging == false) {
     console.log('Logging is disabled');
 } else {
-    if (config.simple == true) {
-        console.log('Simple logging is enabled');
+    if (config.simplifiedLogging == true) {
+        console.log('Simplified Logging is enabled');
     } else {
         console.log('Logging is enabled');
     }
